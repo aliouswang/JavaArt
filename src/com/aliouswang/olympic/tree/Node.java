6a8b66dd3e6dcff93d3fb3228375780b078b3b3f
aliouswang <wsw622@126.com>
2019-03-05 19:12:31 +0800

    Initial commit

---
+++ src/com/aliouswang/olympic/tree/Node.java
@@ -0,0 +1,16 @@
+package com.aliouswang.olympic.tree;
+
+public class Node {
+
+    int key;
+    int value;
+
+    Node left;
+    Node right;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
---
+++ src/com/aliouswang/olympic/tree/Tree.java
@@ -0,0 +1,19 @@
+package com.aliouswang.olympic.tree;
+
+public class Tree {
+
+    Node root;
+
+    public Tree() {
+
+    }
+
+    private void init() {
+
+    }
+
+    public void serach(int key) {
+
+    }
+
+}
