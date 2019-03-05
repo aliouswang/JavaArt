a6ae8d4518c81b5ca4d8e1fdc3fa96665c3eefd4
aliouswang <wsw622@126.com>
2019-03-06 07:21:54 +0800

    add tree find

--- src/com/aliouswang/olympic/tree/Tree.java
+++ src/com/aliouswang/olympic/tree/Tree.java
@@ -12,8 +12,17 @@ public class Tree {
 
     }
 
-    public void serach(int key) {
+    public Node find(int key) {
+        if (root == null) return null;
+        return find(key, root);
+    }
 
+    private Node find(int key, Node startNode) {
+        if (startNode == null) return null;
+        if (startNode.key == key) return startNode;
+        else if (startNode.key > key) {
+            return find(key, startNode.left);
+        }else return find(key, startNode.right);
     }
 
 }
