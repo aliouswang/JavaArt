9f6630f53096f449c103071b575612cbe4a2a6bf
aliouswang <wsw622@126.com>
2019-03-06 19:27:31 +0800

    递归实现 BST

--- src/com/aliouswang/olympic/tree/Tree.java
+++ src/com/aliouswang/olympic/tree/Tree.java
@@ -28,7 +28,70 @@ public class Tree {
     public void insert(int key, int value) {
         if (root == null) {
             root = new Node(key, value);
+            return;
+        }
+        insert(key, value, root);
+    }
+
+    private void insert(int key, int value, Node startNode) {
+        if (key == startNode.key) {
+            startNode.value = value;
+            return;
+        } else if (key < startNode.key) {
+            if (startNode.left == null) {
+                startNode.left = new Node(key, value);
+                return;
+            }else {
+                insert(key, value, startNode.left);
+            }
+        } else if (key > startNode.key) {
+            if (startNode.right == null) {
+                startNode.right = new Node(key, value);
+                return;
+            }else {
+                insert(key, value, startNode.right);
+            }
         }
     }
+
+    public Node findReplaceNode(Node deleteNode) {
+        Node findNode = find(deleteNode.key);
+        //left child is not null
+        if (findNode.left != null) {
+            return findBiggest(findNode.left);
+        }else if (findNode.right != null) {
+            return findSmallest(findNode.right);
+        }else return null;
+    }
+
+    public Node findBiggest(Node startNode) {
+        if (startNode.right == null) return startNode;
+        else return findBiggest(startNode.right);
+    }
+
+    public Node findSmallest(Node startNode) {
+        if (startNode.left == null) return startNode;
+        else return findSmallest(startNode.left);
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    public void preOrder(Node rootNode) {
+        if (rootNode == null) return;
+        System.out.print(rootNode.value + ",");
+        preOrder(rootNode.left);
+        preOrder(rootNode.right);
+    }
+
+    public void midOrder() {
+
+    }
+
+    public void postOrder() {
+
+    }
+
 
 }
--- src/com/aliouswang/olympic/tree/TreeTest.java
+++ src/com/aliouswang/olympic/tree/TreeTest.java
@@ -1,4 +1,25 @@
 package com.aliouswang.olympic.tree;
 
 public class TreeTest {
+
+    public static void main(String[] args) {
+
+        Tree tree = new Tree();
+        tree.insert(6, 6);
+        tree.insert(3, 3);
+        tree.insert(14, 14);
+        tree.insert(10, 10);
+        tree.insert(9, 9);
+        tree.insert(13, 131);
+        tree.insert(11, 11);
+        tree.insert(12, 12);
+        tree.insert(16, 16);
+
+        System.out.println("find key 13 value: " + tree.find(13).value);
+
+        System.out.println("find replace : " + tree.findReplaceNode(new Node(11, 11)).value);
+
+        tree.preOrder();
+    }
+
 }
